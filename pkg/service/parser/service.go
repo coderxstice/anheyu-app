4951371154ffb2683dee9a1f438a6de3b91c9492
anzhiyu-c <me@anheyu.com>
2025-08-27 14:34:54 +0800

    更新服务解析器，新增对 div 元素的 align 属性的支持，进一步增强 HTML 安全策略。

--- pkg/service/parser/service.go
+++ pkg/service/parser/service.go
@@ -72,6 +72,7 @@ func NewService(settingSvc setting.SettingService, bus *event.EventBus) *Service
 		"div", "span", "p", "font", "th", "td", "rect", "blockquote", "img", "h1", "h2", "h3", "h4", "h5", "h6", "a", "strong", "b", "em", "i", "u", "s", "strike", "del", "pre", "code", "sub", "sup", "mark", "ul", "ol", "li", "table", "thead", "tbody", "tfoot", "tr", "section", "article", "header", "footer", "nav", "aside", "main", "hr", "figure", "figcaption", "svg", "path", "circle", "line", "g", "text",
 	)
 	policy.AllowAttrs("color").OnElements("font")
+	policy.AllowAttrs("align").OnElements("div")
 	policy.AllowAttrs("xmlns").OnElements("annotation", "div")
 	policy.AllowAttrs("encoding").OnElements("input")
 	policy.AllowAttrs("type").OnElements("input")
