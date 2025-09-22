ba185d223454aeaec1001fb7db3ff495f86bc323
anzhiyu-c <me@anheyu.com>
2025-09-22 16:37:00 +0800

    更新子项目提交信息，反映最新的提交状态；添加音乐播放器配置选项，支持启用音乐播放器功能。

--- pkg/constant/setting.go
+++ pkg/constant/setting.go
@@ -3,7 +3,7 @@
  * @Description:
  * @Author: 安知鱼
  * @Date: 2025-06-21 17:18:09
- * @LastEditTime: 2025-09-20 17:31:37
+ * @LastEditTime: 2025-09-22 16:17:20
  * @LastEditors: 安知鱼
  */
 package constant
@@ -224,5 +224,7 @@ const (
 	KeyAboutPageCareers              SettingKey = "about.page.careers"
 	KeyAboutPageSkillsTips           SettingKey = "about.page.skills_tips"
 	KeyAboutPageStatisticsBackground SettingKey = "about.page.statistics_background"
-	KeyMusicPlayerPlaylistID         SettingKey = "music.player.playlist_id"
+
+	KeyMusicPlayerEnable     SettingKey = "music.player.enable"
+	KeyMusicPlayerPlaylistID SettingKey = "music.player.playlist_id"
 )
