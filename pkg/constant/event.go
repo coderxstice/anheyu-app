5a759fd8b77b2559471a92f43c8dea58e596de05
anzhiyu-c <me@anheyu.com>
2025-10-09 18:41:12 +0800

    feat(app): 添加EntityRepository和EventBus到App结构，增强应用功能

---
+++ pkg/constant/event.go
@@ -0,0 +1,19 @@
+/*
+ * @Description:
+ * @Author: 安知鱼
+ * @Date: 2025-10-09 18:07:37
+ * @LastEditTime: 2025-10-09 18:07:49
+ * @LastEditors: 安知鱼
+ */
+package constant
+
+import "github.com/anzhiyu-c/anheyu-app/internal/pkg/event"
+
+// EventTopic 事件主题类型
+type EventTopic = event.Topic
+
+// 导出事件主题常量，供外部使用
+const (
+	// EventFileCreated 文件创建事件
+	EventFileCreated EventTopic = event.FileCreated
+)
